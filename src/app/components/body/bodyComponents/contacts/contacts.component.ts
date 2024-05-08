6ae9e36ff77d8793f0a2a64546b82aeb186df6d4
Diana Makovetska <diana.makovetska@klr.com.ua>
2024-05-08 16:15:09 +0300

    fix contacts map leaflet

--- src/app/components/body/bodyComponents/contacts/contacts.component.ts
+++ src/app/components/body/bodyComponents/contacts/contacts.component.ts
@@ -1,44 +1,48 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import * as L from 'leaflet';
+import {AfterViewInit, Component, Inject, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit,AfterViewInit {
+export class ContactsComponent implements AfterViewInit {
   private map: any;
-  private centroid: L.LatLngExpression = [49.827589532104774, 23.991570501440744];
-  icon = {
-    icon: L.icon({
-      iconSize: [ 25, 41 ],
-      iconAnchor: [ 13, 41 ],
-      iconUrl: 'assets/marker-icon.png',
-      shadowUrl: 'assets/marker-shadow.png'
-    })
-  };
+  private centroid: any;
+  private icon: any;
 
-  private initMap(): void {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {
+  }
 
+  async ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Dynamically import Leaflet only if in browser environment
+      const L = await import('leaflet');
+      this.centroid = [49.827589532104774, 23.991570501440744];
+      this.icon = L.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      });
+      this.initMap(L);
+    }
+  }
+
+  private initMap(L: any): void {
     this.map = L.map('map', {
       center: this.centroid,
       zoom: 17
     });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 20,
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    }).addTo(this.map);
 
-    tiles.addTo(this.map);
-    const marker = L.marker([49.827589532104774, 23.991570501440744], this.icon).addTo(this.map);
-  }
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  ngAfterViewInit(): void {
-    this.initMap();
-
+    L.marker(this.centroid, {icon: this.icon}).addTo(this.map);
   }
 }
